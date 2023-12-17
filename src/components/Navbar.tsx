c8ac8d3e9242ff5e23b2083a6c9556c7f8ca0b73
Jackson <jackson.h.rudd@gmail.com>
2023-12-17 09:04:04 -0800

    done

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+function NavBar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <a className="navbar-brand" href="/">
+        Qlab
+      </a>
+    </nav>
+  );
+}
+
+export default NavBar;
