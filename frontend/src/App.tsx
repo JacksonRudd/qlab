c0e387310ebd12300f8f114734c593777cec6993
Jackson <jackson.h.rudd@gmail.com>
2023-12-24 05:12:46 -0800

    added next button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,38 +3,32 @@ import Navbar from "./components/Navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Quiz from "./components/Quiz";
 import getProviders from "./providers/axios_provider";
+import { useState } from "react";
+import Question from "./components/Question";
 
-// const get_question = (topic: string): Promise<QuestionData> => {
-//   return fetchData("http://localhost:8080/question/" + topic)("");
-// };
-
-// const get_explanation = async (
-//   question: string,
-//   user_answer: string
-// ): Promise<Explanation> => {
-//   return {
-//     is_correct: true,
-//     explanation: `You answered '${user_answer}', and that is correct for all kinds of reasons to the question '${question}'.`,
-//   };
-// };
-
-// const mock_get_explanation = async (
-//   question: string,
-//   user_answer: string
-// ): Promise<Explanation> => {
-//   return {
-//     is_correct: true,
-//     explanation: `You answered '${user_answer}', and that is correct for all kinds of reasons to the question '${question}'.`,
-//   };
-// };
 const url = "http://localhost:8080";
 const { sendAnswer, fetchQuestion } = getProviders(url);
 
 function App() {
+  const [topic, setTopic] = useState<string | null>(null);
+
   return (
     <>
       <Navbar />
-      <Quiz get_question={fetchQuestion} get_explanation={sendAnswer} />
+      {topic == null && (
+        <Question
+          title={"Choose your topic."}
+          content={"Put any topic you want to be tested on."}
+          processUserAnswer={setTopic}
+        ></Question>
+      )}
+      {topic && (
+        <Quiz
+          get_question={fetchQuestion}
+          get_explanation={sendAnswer}
+          topic={topic}
+        />
+      )}
     </>
   );
 }
--- frontend/src/components/Quiz.tsx
+++ frontend/src/components/Quiz.tsx
@@ -5,6 +5,7 @@ import Question from "./Question";
 import Answer from "./Answer";
 
 interface QuizProps {
+  topic: string;
   get_question: (topic: string) => Promise<QuestionData>;
   get_explanation: (
     question: string,
@@ -12,29 +13,40 @@ interface QuizProps {
   ) => Promise<Explanation>;
 }
 
-function Quiz({ get_question, get_explanation }: QuizProps) {
+function Quiz({ get_question, get_explanation, topic }: QuizProps) {
   const [questionData, setQuestionData] = useState<QuestionData | null>(null);
-  const [useranswer, setUserAnswer] = useState<string | null>(null);
   const [explanation, setExplanation] = useState<string | null>(null);
   const [isUserCorrect, setCorrect] = useState<boolean | null>(null);
+  const [isLoadingAnswer, setLoadingAnswer] = useState(false);
+  const [isLoadingQuestion, setLoadingQuestion] = useState(false);
 
-  let topic = "christmas history";
-  useEffect(() => {
+  const get_next_question = () => {
+    setLoadingQuestion(true);
+    setQuestionData(null);
     get_question(topic)
       .then((data) => {
         setQuestionData(data);
       })
       .catch((error) => console.error("Error fetching data:", error));
+    setCorrect(null);
+    setLoadingAnswer(false);
+    setExplanation(null);
+    setLoadingQuestion(false);
+  };
+
+  useEffect(() => {
+    get_next_question();
   }, []);
 
   const handleSubmit = (answer: string) => {
-    setUserAnswer(answer);
+    setLoadingAnswer(true);
     if (questionData && questionData.content) {
       // Get the explanation
       get_explanation(questionData.content, answer).then(
         (data: Explanation) => {
           setCorrect(data.is_correct);
           setExplanation(data.explanation);
+          setLoadingAnswer(false);
         }
       );
     }
@@ -42,6 +54,7 @@ function Quiz({ get_question, get_explanation }: QuizProps) {
 
   return (
     <>
+      {isLoadingQuestion && <h1>Loading... </h1>}
       {questionData && (
         <Question
           title={"Question"}
@@ -49,9 +62,19 @@ function Quiz({ get_question, get_explanation }: QuizProps) {
           processUserAnswer={handleSubmit}
         />
       )}
+      {isLoadingAnswer && <div className="loading-icon">Loading...</div>}
 
       {explanation != null && isUserCorrect != null && (
-        <Answer explanation={explanation} isCorrect={isUserCorrect}></Answer>
+        <>
+          <Answer explanation={explanation} isCorrect={isUserCorrect}></Answer>
+          <button
+            type="submit"
+            className="btn btn-primary"
+            onClick={get_next_question}
+          >
+            Next
+          </button>
+        </>
       )}
     </>
   );
